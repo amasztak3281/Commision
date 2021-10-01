22c93d9890b15add52d95b586153e0f4e9236dfc
Alexander Masztak <amasztak3281@stu.d214.org>
2021-10-01 14:43:02 -0500

    IBoutlet + Action

--- Commision/ViewController.swift
+++ Commision/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var BasePayLabel: UILabel!
+    @IBOutlet weak var CommisionPayTextField: UITextField!
+    @IBOutlet weak var TotalPayLabel: UILabel!
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
-
+    @IBAction func WhenButtonPressed(_ sender: Any)
+    {
+        
+        
+    }
+    
 }
 
